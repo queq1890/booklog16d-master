5e1d237d54de3a254aa83a6f4cdeb8b24f763a4c
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-05-28 09:00:06 +0900

    fix repo name

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.10.0"
 
 set :branch, "deploy-branch"
 set :application, "booklog16d"
-set :repo_url, "git://github.com/Motohide/booklog16d.git"
+set :repo_url, "git://github.com/queq1890/booklog16d-master.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -11,7 +11,8 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/motohide/.ssh/hahaha.pem']
+# TODO ここ書き換える
+                  keys: ['/Users/yujimatsumoto/.ssh/first_key_tloz.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
